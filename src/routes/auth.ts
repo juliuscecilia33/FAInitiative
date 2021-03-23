0ad18a342508dbe528620e975191cd073a408de9
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-03-23 15:05:42 -0700

    login validation complete

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { validate } from "class-validator";
+import { isEmpty, validate } from "class-validator";
 import bcrypt from "bcrypt";
 
 import { User } from "../entities/User";
@@ -42,6 +42,15 @@ const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
   try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
     const user = await User.findOne({ username });
 
     if (!user) return res.status(404).json({ error: "User not found" });

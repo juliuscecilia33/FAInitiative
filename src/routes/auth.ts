4e6d3c8fa2e599797469e8734db17513256a9746
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-03-23 15:03:13 -0700

    login setup

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { validate } from "class-validator";
+import bcrypt from "bcrypt";
 
 import { User } from "../entities/User";
 
@@ -37,7 +38,26 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return res.status(401).json({ password: "Password is incorrect" });
+    }
+
+    return res.json(user);
+  } catch (err) {}
+};
+
 const router = Router();
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;

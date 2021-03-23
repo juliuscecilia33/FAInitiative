ec0d0a2f399f30d40af9b46a3f13ea7ee1fd274d
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-03-22 18:43:34 -0700

    express setup and more dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,22 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entities/User";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(5000, async () => {
+  console.log("Server running at http://localhost:5000");
+
+  try {
+    await createConnection();
+    console.log("Database connected!");
+  } catch (err) {
+    console.log(err);
+  }
+});

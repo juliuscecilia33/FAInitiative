cdc3a3724fdfcd62aba39c1f463bbbfdd1f06a2a
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-03-26 09:59:54 -0700

    added ts configs and fixed errors

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -64,7 +64,7 @@ const login = async (req: Request, res: Response) => {
       return res.status(401).json({ password: "Password is incorrect" });
     }
 
-    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+    const token = jwt.sign({ username }, process.env.JWT_SECRET!);
 
     res.set(
       "Set-Cookie",
@@ -78,7 +78,10 @@ const login = async (req: Request, res: Response) => {
     );
 
     return res.json(user);
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "Something went wrong" });
+  }
 };
 
 const me = (_: Request, res: Response) => {

78ea94cf29d0417abbc4b0e570111a422d707fdb
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-03-27 12:42:59 -0700

    logo add and tailwind setup

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,17 +1,18 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Register() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col">
       <Head>
-        <title>Create Next App</title>
+        <title>Register</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Register Page</h1>
-      </main>
+      <div className="w-full flex justify-center items-center h-7v bg-white border-b border-primary">
+        <img className="h-auto" src="/images/Logo.png" />
+      </div>
+
+      <div className="w-full h-93v bg-red-500 flex"></div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,42 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
+    height: {
+      "7v": "7vh",
+      "8v": "8vh",
+      "9v": "9vh",
+      "20v": "20vh",
+      "30v": "30vh",
+      "40v": "40vh",
+      "50v": "50vh",
+      "60v": "60vh",
+      "70v": "70vh",
+      "80v": "80vh",
+      "90v": "90vh",
+      "93v": "93vh",
+      "100v": "100vh",
+    },
+    borderColor: (theme) => ({
+      ...theme("colors"),
+      DEFAULT: theme("colors.gray.300", "currentColor"),
+      primary: "#E8F1F2",
+      secondary: "#ffed4a",
+      danger: "#e3342f",
+    }),
   },
   variants: {
     extend: {},

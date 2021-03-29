aae61c53fd4d8623169e3372ae30be0a4c78ead4
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-03-29 12:24:25 -0700

    cors keep cookie token

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import Axios from "axios";
 
 Axios.defaults.baseURL = "http://localhost:5000/api";
+Axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

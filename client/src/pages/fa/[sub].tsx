a5b386bb3619d74594185eb05362470db6ee97ff
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-04-07 07:35:43 -0700

    fixed sub posts sizing

--- client/src/pages/fa/[sub].tsx
+++ client/src/pages/fa/[sub].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import PostCard from "../../components/PostCard";
+import Sidebar from "../../components/Sidebar";
 
 export default function Sub() {
   const router = useRouter();
@@ -10,14 +11,17 @@ export default function Sub() {
   const { data: sub } = useSWR(subName ? `/subs/${subName}` : null);
 
   return (
-    <div className="flex flex-col items-center w-full h-full pt-8 bg-transparent">
-      {sub && (
-        <div>
-          {sub.posts.map((post) => (
-            <PostCard key={post.id} post={post} />
-          ))}
-        </div>
-      )}
+    <div className="flex w-full h-92v">
+      <Sidebar />
+      <div className="flex flex-col items-center w-full h-full pt-8 bg-transparent">
+        {sub && (
+          <div className="flex flex-col items-center justify-center w-full">
+            {sub.posts.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }

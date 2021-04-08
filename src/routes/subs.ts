dd636618d980d70cc6161fb863ec9d095cc91ea1
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-04-08 07:46:19 -0700

    sub image upload and filtering

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,12 +1,15 @@
 import { Request, Response, Router } from "express";
 import { isEmpty } from "class-validator";
 import { getRepository } from "typeorm";
+import multer, { FileFilterCallback } from "multer";
+import path from "path";
 
 import User from "../entities/User";
 import Sub from "../entities/Sub";
 import auth from "../middleware/auth";
 import user from "../middleware/user";
 import Post from "../entities/Post";
+import { makeId } from "../util/helpers";
 
 const createSub = async (req: Request, res: Response) => {
   const { name, title, description } = req.body;
@@ -68,9 +71,31 @@ const getSub = async (req: Request, res: Response) => {
   }
 };
 
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "public/images",
+    filename: (_, file, callback) => {
+      const name = makeId(15);
+      callback(null, name + path.extname(file.originalname)); // e.g. image + .png
+    },
+  }),
+  fileFilter: (_, file, callback: FileFilterCallback) => {
+    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+      callback(null, true);
+    } else {
+      callback(new Error("Not an image"));
+    }
+  },
+});
+
+const uploadSubImage = async (_: Request, res: Response) => {
+  return res.json({ success: true });
+};
+
 const router = Router();
 
 router.post("/", user, auth, createSub);
 router.get("/:name", user, getSub);
+router.post("/:name/image", user, auth, upload.single("file"), uploadSubImage);
 
 export default router;

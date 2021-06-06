42d5b067bbf2df1253cf11280cd169bdc9530040
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-06-06 07:59:03 -0700

    redirect if not logged in

--- client/src/pages/fa/[sub]/submit.tsx
+++ client/src/pages/fa/[sub]/submit.tsx
@@ -10,6 +10,7 @@ import InputGroup from "../../../components/InputGroup";
 import { FormEvent, useState } from "react";
 import TextAreaGroup from "../../../components/TextAreaGroup";
 import Axios from "axios";
+import { GetServerSideProps } from "next";
 
 export default function submit() {
   const router = useRouter();
@@ -120,3 +121,16 @@ export default function submit() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) throw new Error("Missing auth token cookie");
+
+    await Axios.get("/auth/me", { headers: { cookie } });
+
+    return { props: {} };
+  } catch (err) {
+    res.writeHead(307, { Location: "/login" }).end(); // status codes w 300 are redirect
+  }
+};

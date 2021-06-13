51308ddd8b341af370318d56ff4ca39aee038721
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-06-13 15:49:12 -0700

    button fix

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 const Sidebar: React.FC = () => (
   <div className="fixed flex flex-col items-center content-between h-full py-5 border-r border-primary w-1/18">
     <div className="fixed flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full bg-secondary">
+      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full cursor-pointer bg-secondary">
         <i className="h-auto text-xl fas fa-home text-secondary"></i>
       </div>
-      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full bg-secondary">
+      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full cursor-pointer bg-secondary">
         <i className="h-auto text-xl fas fa-plus text-secondary"></i>
       </div>
-      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full bg-secondary">
+      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full cursor-pointer bg-secondary">
         <i className="h-auto text-xl fas fa-users text-secondary"></i>
       </div>
-      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full bg-secondary">
+      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full cursor-pointer bg-secondary">
         <i className="h-auto text-xl fas fa-sign-out-alt text-secondary"></i>
       </div>
-      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full bg-secondary">
+      <div className="flex items-center justify-center w-12 h-12 mb-12 rounded-full cursor-pointer bg-secondary">
         <i className="h-auto text-xl fas fa-id-card-alt text-secondary"></i>
       </div>
     </div>
